598768e79d317fec2421e913963a1ead0aea89c3
isif00 <sifodev@gmail.com>
2025-06-10 23:21:40 +0100

    [wip]: tx feature

--- internal/domain/tx/tx.go
+++ internal/domain/tx/tx.go
@@ -1,5 +1,12 @@
 package tx
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/gob"
+	"fmt"
+)
+
 type TxInput struct {
 	TxID      string
 	OutputIdx int
@@ -16,4 +23,37 @@ type Transaction struct {
 	ID      string
 	Inputs  []TxInput
 	Outputs []TxOutput
+}
+
+func NewCoinbaseTx(to string, reward int) (*Transaction, error) {
+	output := TxOutput{
+		Amount:     reward,
+		PubKeyHash: []byte(to),
+	}
+	tx := &Transaction{
+		Inputs:  []TxInput{},
+		Outputs: []TxOutput{output},
+	}
+
+	id, err := tx.Hash()
+	if err != nil {
+		return nil, err
+	}
+	tx.ID = id
+
+	return tx, nil
+}
+
+func (tx *Transaction) Hash() (string, error) {
+	var buf bytes.Buffer
+
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(tx)
+	if err != nil {
+		return "", err
+	}
+
+	hash := sha256.Sum256(buf.Bytes())
+
+	return fmt.Sprintf("%x", hash[:]), nil
 }
